24d63b50e3ca969c68e871bb84a81194b7e7b51e
asifalix <azifali@gmail.com>
2023-01-03 22:52:10 -0800

    updated .well-known did

---
+++ _site/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+      './_drafts/**/*.html',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.md',
+      './*.html',
+      'node_modules/preline/dist/*.js',
+    ],
+    theme: {
+      
+      theme: {
+        
+        extend: {},
+      },
+    },
+    plugins: [require('preline/plugin'),]
+  }
