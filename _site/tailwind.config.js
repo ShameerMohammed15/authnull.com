da1557b680e4121b8fcc73e3d268c27319dd399a
asifalix <azifali@gmail.com>
2023-07-20 09:32:07 -0700

    updated with site
    Former-commit-id: 2210b27da6954a1b15d4809ec881de704b6c33e5

---
+++ _site/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+      './_drafts/**/*.html',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.md',
+      './*.html',
+      'node_modules/preline/dist/*.js',
+    ],
+    theme: {
+      
+      theme: {
+        
+        extend: {},
+      },
+    },
+    plugins: [require('preline/plugin'),
+    require('flowbite-typography'),]
+    
+  }
