fc2617ace84201831b603c3e8448b558f8122502
asifalix <azifali@gmail.com>
2023-11-29 12:14:29 -0800

    updated blogs

--- _site/blog/assets/js/data/swcache.js
+++ _site/blog/assets/js/data/swcache.js
@@ -45,7 +45,7 @@ const allowedDomains = [
     'www.google-analytics.com',
   
 
-  'localhost:4000',
+  'authnull.com',
 
   
 
--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701286174';
+const cacheName = 'chirpy-1701288854';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
