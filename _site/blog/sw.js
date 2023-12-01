817faff8ddf06f0179846e41fbe67d231a4245d9
asifalix <azifali@gmail.com>
2023-12-01 09:10:04 -0800

    updated home page and blog

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701289171';
+const cacheName = 'chirpy-1701450546';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
---
+++ contactus2.md
@@ -0,0 +1,5 @@
+---
+layout: contactus2
+title: "Contact Us"
+---
+<!-- <script src="{{ "/node_modules/preline/dist/preline.js" | relative_url }}"></script> -->
