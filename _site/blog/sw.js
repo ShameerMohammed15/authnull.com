f7d768d912103dca532b9464c124db7872bf0e16
asifalix <azifali@gmail.com>
2023-12-13 18:19:46 -0800

    updated reddit tracking code

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701732886';
+const cacheName = 'chirpy-1702520341';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- responsible-disclosure.md
+++ responsible-disclosure.md
@@ -1,5 +1,5 @@
 ---
-layout: why-authnull
-title: "Why AuthNull"
+layout: disclosure
+title: "Responsible Disclosure"
 ---
 <!-- <script src="{{ "/node_modules/preline/dist/preline.js" | relative_url }}"></script> -->x
