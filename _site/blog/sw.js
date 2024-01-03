9d647d877487bc3fe05d94dbc18f5db2b17ec25a
asifalix <azifali@gmail.com>
2024-01-02 23:37:01 -0800

    updated home page and missing blogs

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704267222';
+const cacheName = 'chirpy-1704267355';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
