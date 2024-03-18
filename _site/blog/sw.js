e5b86b0015f8bd41def012c792110c1f61f49bcb
asifalix <azifali@gmail.com>
2024-03-18 08:48:00 -0700

    updated privacy policy link on the footer

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710776699';
+const cacheName = 'chirpy-1710776864';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
