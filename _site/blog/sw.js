f5d67e663b9470d9650100eff282d109054bb9c0
asifalix <azifali@gmail.com>
2024-05-01 15:21:12 -0700

    updates

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1714548157';
+const cacheName = 'chirpy-1714602018';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
