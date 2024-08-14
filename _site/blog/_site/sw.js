ee7ad275b67443b311aaa19e8fc108ece49bea41
asifalix <azifali@gmail.com>
2024-08-13 17:30:14 -0700

    Simplified website structure

--- _site/blog/_site/sw.js
+++ _site/blog/_site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1723579408';
+const cacheName = 'chirpy-1723595372';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
